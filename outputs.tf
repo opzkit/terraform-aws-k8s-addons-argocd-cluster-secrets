2fd401ec98313b1fcb934c97aaa18abf684b4033
Joakim Olsson <joakim@unbound.se>
2025-10-07 16:23:11 +0200

    fix(outputs): update output format for addons to list
    Change the output format of the "addons" variable from a map to a list  
    to enhance consistency and facilitate easier iteration. This change  
    ensures compatibility with other modules that expect a list structure.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "addons" {
-  value = {
+  value = [{
     content = templatefile("${path.module}/addon_content.tpl", {
       secrets = [for s in data.aws_secretsmanager_secret_version.secret_version : jsondecode(s.secret_string)]
     })
     version = local.version
     name    = "argocd-cluster-secrets"
-  }
+  }]
 }
